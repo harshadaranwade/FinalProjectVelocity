e73dc74b1d8c4bcc20626c0460800c222035c884
Prajakta <shindeprajakta1425@gmail.com>
2023-06-26 22:56:07 +0530

    changes added

--- src/app/FeaturedModule/categories/subcategories/subcategories.module.ts
+++ src/app/FeaturedModule/categories/subcategories/subcategories.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubcategoriesComponent } from './subcategories.component';
+import { AddCategorylistComponent } from './add-categorylist/add-categorylist.component';
+import { SubCategorylistComponent } from './sub-categorylist/sub-categorylist.component';
 
 
 @NgModule({
   declarations: [
-    SubcategoriesComponent
+    SubcategoriesComponent,
+    AddCategorylistComponent,
+    SubCategorylistComponent
   ],
   imports: [
     CommonModule
